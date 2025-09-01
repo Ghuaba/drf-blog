94be173ef57775afde116e87d687aa21d207bbea
ghuaba <pool.ochoa.robalino@gmail.com>
2025-09-01 00:28:31 -0500

    feat: implement PostList, PostDetail, and PostHeadings views using APIView
    - Added APIView-based endpoints for listing posts, retrieving post details by slug,
      and retrieving all headings for a given post.
    - Replaced direct URL parameter usage with self.kwargs.get('slug') to avoid TypeError.
    - Preserved previous GenericAPIView-based implementations as comments for reference.

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,26 +1,55 @@
 from rest_framework.generics import ListAPIView, RetrieveAPIView
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+
 from .models import Post, Heading
 from .serializers import PostListSerializer, PostSerializer, HeadingSerializer
 
 
-#La clase mas sencilla con ListAPIView, mas automatico
-class PostListView(ListAPIView):
-    #para mostrar solo los publicados , POstObjects creado por mi que se le asigan a la varaible post_objects
-    queryset = Post.post_objects.all()#todos los posts publicos
-    serializer_class = PostListSerializer
+#Uso con APIView
+class PostListView(APIView):
+    def get(self, request, *args, **kwargs):
+        posts = Post.post_objects.all()
+        serialized_posts = PostListSerializer(posts, many=True).data
+        return Response(serialized_posts)
 
 
-class PostDetailView(RetrieveAPIView):
-    queryset = Post.post_objects.all()#todos los posts publicos : post_objects es la variable
-    serializer_class = PostSerializer
-    lookup_field = 'slug' #esto indica que el campo para buscar será `slug`
+class PostDetailView(APIView):
+    def get(self, request, *args, **kwargs):
+        slug = self.kwargs.get('slug')
+        post = Post.post_objects.get(slug=slug)
+        serialized_post = PostSerializer(post).data
+        return Response(serialized_post)
+
+
+class PostHeadingsView(APIView):
+    def get(self, request, *args, **kwargs):
+        post_slug = self.kwargs.get('slug')
+        headings = Heading.objects.filter(post__slug=post_slug)
+        serialized_headings = HeadingSerializer(headings, many=True).data
+        return Response(serialized_headings)
+
+
+#Realizado con GenerisAPIView
+# #La clase mas sencilla con ListAPIView, mas automatico
+# class PostListView(ListAPIView):
+#     #para mostrar solo los publicados , POstObjects creado por mi que se le asigan a la varaible post_objects
+#     queryset = Post.post_objects.all()#todos los posts publicos
+#     serializer_class = PostListSerializer
+
+
+# class PostDetailView(RetrieveAPIView):
+#     queryset = Post.post_objects.all()#todos los posts publicos : post_objects es la variable
+#     serializer_class = PostSerializer
+#     lookup_field = 'slug' #esto indica que el campo para buscar será `slug`
 
 #SOlamente para otra forma de unir con headings para el post, solo llamamos a la api los headers a partir del slug
-class PostHeadingsView(ListAPIView):
-    serializer_class = HeadingSerializer
+# class PostHeadingsView(ListAPIView):
+#     serializer_class = HeadingSerializer
 
-    def get_queryset(self):
-        post_slug = self.kwargs.get("slug")
-        return Heading.objects.filter(post__slug=post_slug)
+#     def get_queryset(self):
+#         post_slug = self.kwargs.get("slug")
+#         return Heading.objects.filter(post__slug=post_slug)
 
         
