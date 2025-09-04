2d435f624e4e650d17bdf3278509b00980cb9725
ghuaba <pool.ochoa.robalino@gmail.com>
2025-09-04 14:09:13 -0500

    feat(posts): add error handling for Post views and analytics updates

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,30 +1,51 @@
 from rest_framework.generics import ListAPIView, RetrieveAPIView
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from .utils import get_client_ip
+from rest_framework.exceptions import NotFound, APIException
 
 from .models import Post, Heading, PostView, PostAnalytics
 from .serializers import PostListSerializer, PostSerializer, HeadingSerializer, ViewPostSerializer
+from .utils import get_client_ip
 
 
-#Uso con APIView
+#Uso con APIView mas control
 class PostListView(APIView):
     def get(self, request, *args, **kwargs):
-        posts = Post.post_objects.all()
-        serialized_posts = PostListSerializer(posts, many=True).data
+        try:
+            posts = Post.post_objects.all()
+            if not posts.exists():
+                raise NotFound(detail="No posts found")
+            
+            serialized_posts = PostListSerializer(posts, many=True).data
+        except Post.DoesNotExist:
+            raise NotFound(detail="No posts found")
+        except Exception as e:
+            raise APIException(detail=f"An unexpected error ocurred: {str(e)}")
+        
         return Response(serialized_posts)
 
 
 class PostDetailView(APIView):
     def get(self, request, *args, **kwargs):
-        slug = self.kwargs.get('slug')
-        post = Post.post_objects.get(slug=slug)
+        try:
+            #FOrma correcemtne de obetener el slug a partir de los Kwargs
+            slug = self.kwargs.get('slug')
+            post = Post.post_objects.get(slug=slug)
+        except Post.DoesNotExist:
+            raise NotFound(detail="The requested publicacion/post does not exist")
+        except Exception as e:
+            raise APIException(detail=f"An unexpected error ocurred: {str(e)}")
+        
         serialized_post = PostSerializer(post).data
         #Para hacer conteo de visualizaciones
         #Incrementa post view count
-        post_analytics = PostAnalytics.objects.get(post=post)
-        post_analytics.increment_view(request)
-
+        try:
+            post_analytics = PostAnalytics.objects.get(post=post)
+            post_analytics.increment_view(request)
+        except PostAnalytics.DoesNotExist:
+            raise NotFound(detail="Analytics data for thuis post does not exist")
+        except Exception as e:
+            raise APIException(detail=f"An error ocurred while updateing post analytics: {str(e)}")
         return Response(serialized_post)
 
 
